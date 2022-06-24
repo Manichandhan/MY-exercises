6b33cf9c6ba1ca145abc300234175b1f1e8738c7
Manichandhan <108020361+Manichandhan@users.noreply.github.com>
2022-06-24 22:40:49 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,151 @@
+//let a=3
+//a=(a++,++a,+a)
+//console.log(a)
+//console.log(a++,a)
+//console.log(!a)
+/*let traffic_light = "red"
+if(traffic_light=="red")
+{
+    console.log("stop the car")
+}
+else
+{
+    console.log("keep going")
+}*/
+
+/*let NUM = 15
+if(NUM < 15)
+{
+    console.log("NUM is less than 15")
+}
+else if(NUM > 15)
+{
+    console.log("NUM IS GREATER IS THAN 15")
+}
+else{
+    console.log("NUM IS EQUAL TO 15")
+}*/
+/*let a = 5
+let b = 6
+if(a==b)
+{
+    console.log("a is equal to b")
+}
+else{
+    console.log("a is not equal to b")
+    if(a>b)
+    {
+        console.log("a is greater than b")
+    }
+    else
+    {
+        console.log("a is less than b")
+    }
+}*/
+/*let choice = "5"
+switch(choice)
+{
+    case "e":
+        console.log("u have chosen e")
+        break;
+    case "j":
+        console.log("u have chosen j")    
+    
+        default:
+        console.log("invalid input")
+}*/
+//  const a = 12
+//  const b = 25
+//  const c = 87
+//  if(a<b && b<c)
+//  {
+//    console.log("increasing order")
+//  }
+//  else if(a>b && b>c)
+//  {
+//  console.log("decreasing order")
+// }
+// else{
+//     console.log("neither increaing nor decreasing")
+// }
+// let a = 25
+// let b = 15
+// let c = 20
+// if (a<b && a<c){
+//     console.log("a is the smallest",a)
+// }
+// else if (b<c && b<a){
+//     console.log("b is the smallest",b)
+// }
+// else{
+//     console.log("c is the smallest",c)
+// }
+// const M =7
+// const N = 6
+// let required_classes = Math.ceil(M*.75)
+// if(N>=required_classes)
+// {
+//     console.log(0)
+// }
+// else{
+//     console.log(required_classes-N)
+// }
+// let a = 5
+// console.log(a,a++,a) //postfix increment
+// console.log(++a,a)
+// let a = 6
+// let  b = a++
+// let c = a
+// console.log(a,b,c)
+// function teacher(){
+//     return "Teachers";
+// }
+  
+// function student(){
+//     return "Students";
+// }
+  
+// function greet(user){
+//     console.log(5+6, user());    
+// }
+  
+// // Prints "Welcome Teacher"
+// var message = greet(teacher);
+  
+// // Prints "Welcome Student" 
+// var message = greet(student);
+// let choice = "green"
+// switch (choice){
+//     case "Red": {
+//         console.log("hello")
+//     }
+//     break;
+//     case "green": {
+//         console.log("i am chandu")
+//     }
+// }
+// let a = 9
+// let b = 12
+// let c = a<b?"a is less than b ":"negitive"
+// console.log(c)
+
+// function sumofoddnumbers(N)
+// {
+//     let sum = 0
+//     for(i=1;i<=N;i++)
+//     {
+//         if(i%2!==0){
+//             sum += i
+//         }
+    //   return Sum
+//     }
+      
+// }
+// console.log(typeof sumofoddnumbers(5))
+
+// const mac = "steve"
+// let ios = mac
+// console.log(ios)
+// ios = "steve jobs"
+// console.log(ios)
+
